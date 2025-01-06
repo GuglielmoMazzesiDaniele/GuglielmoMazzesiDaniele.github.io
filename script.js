3b7cf436b86b8bebfb0e078a541a2485f0752cd5
Guglielmo Mazzesi <mazzesi.guglielmo@gmail.com>
2025-01-06 21:01:44 +0100

    Background

--- script.js
+++ script.js
@@ -10,4 +10,26 @@ function loadHomepage() {
         // Appending the button
         nav_bar.appendChild(html_button);
     })
+
+    generateNoise()
+}
+
+// Generating the noise for the
+function generateNoise(){
+
+    const canvas = document.querySelector(".background");
+    const ctx = canvas.getContext("2d");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    const image = ctx.createImageData(canvas.width, canvas.height);
+
+    for(let i = 0; i < image.data.length; i++){
+        image.data[i] = Math.random() * 255;
+        image.data[i + 1] = Math.random() * 255;
+        image.data[i + 2] = Math.random() * 255;
+        image.data[i + 3] = 20;
+    }
+
+    ctx.putImageData(image, 0, 0);
 }
