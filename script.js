3e7cb54361daea19e61ce134d7693650b596b69b
Guglielmo Mazzesi <mazzesi.guglielmo@gmail.com>
2025-01-06 19:15:37 +0100

    Started working on the webpage.

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-// Add any JavaScript you want here for interactivity.
-console.log("Welcome to my portfolio!");
+// Loads the content of the homepage dynamically on load
+function loadHomepage() {
+    let nav_bar = document.getElementById("nav_bar");
+
+    navigations_buttons.forEach((button) => {
+        // Creating the button
+        let html_button = document.createElement("button");
+        // Extracting the value from the array
+        html_button.textContent = button.text;
+        // Appending the button
+        nav_bar.appendChild(html_button);
+    })
+}
---
+++ scripts/constants.js
@@ -0,0 +1,5 @@
+// Array containing the buttons generated on page load
+const navigations_buttons = [
+    {text: "First button", link: ""},
+    {text: "Second button", link: ""},
+];
