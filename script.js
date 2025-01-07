46ab7459392599364d1f3068c7500a84ef3cf2c0
Guglielmo Mazzesi <mazzesi.guglielmo@gmail.com>
2025-01-07 16:31:40 +0100

    Increased background randomness, started working on the main layout.

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+let nav_button = [
+    "Home",
+    "Projects",
+    "Curriculum Vitae",
+]
+
 // Loads the content of the homepage dynamically on load
 function loadHomepage() {
-    let test = ["Test 1", "Test 2"];
-    test.forEach((element) => {
+    nav_button.forEach((element) => {
         let nav_bar = document.querySelector("nav");
         let button = document.createElement("button");
         button.textContent = element;
