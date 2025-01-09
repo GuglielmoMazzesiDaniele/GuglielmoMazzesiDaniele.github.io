611fca6392ed79f092b52bcb0b047ab8a8bfa6de
Guglielmo Mazzesi <mazzesi.guglielmo@gmail.com>
2025-01-09 14:23:53 +0100

    Implemented a basic card.

--- script.js
+++ script.js
@@ -1,42 +1,49 @@
-let interactive_nav_buttons = [
+// Main containers
+let main_containers = [
     {
         text: "Home",
-        container_id: "home",
+        container_id: "home_container",
+        container: null,
         button: null
     },
     {
         text: "Projects",
-        container_id: "projects",
+        container_id: "projects_container",
+        container: null,
         button: null
     },
 ]
 
 // Loads the content of the homepage dynamically on load
 function loadHomepage() {
-    // Loading the buttons
-    interactive_nav_buttons.forEach((element) => {
+    // Creating the main containers logics
+    main_containers.forEach((container) => {
         // Querying the DOM for the navbar
         let nav_bar = document.querySelector("nav");
+        // Querying the DOM for the container
+        container.container = document.getElementById(container.container_id);
+        // Removing the container from the DOM unless it is the homepage
+        if(container.container_id !== "home_container")
+            container.container.remove();
         // Creating a new button
         let button = document.createElement("button");
+        // Saving a reference to the button and appending it in the navbar
+        container.button = button;
+        nav_bar.appendChild(button);
         // Filling the button with the text
-        button.textContent = element.text;
+        button.textContent = container.text;
         // Creating the button listener
         button.addEventListener("click", function(event) {
             // Preventing default behaviour
-            event.preventDefault();
+            event.preventDefault()
             // Hiding all the main containers
-            document.querySelectorAll("main div").forEach((element) => {
-                element.style.display = "none";
+            main_containers.forEach((container) => {
+                container.container.remove();
             })
-            console.log(element.container_id)
-            // Showing the container linked with the button
-            document.getElementById(element.container_id).style.display = "flex";
+            // Pushing the correct container in the DOM
+            document.querySelector("main").appendChild(container.container);
+            console.log(container.container);
         }.bind(this));
-
-        // Saving a reference to the button and appending it in the navbar
-        element.button = button;
-        nav_bar.appendChild(button);
     })
 }
 
